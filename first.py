e23de9154bd280463c54a4c8c58163d233b4289d
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-12-01 23:40:04 +0530

    code updated

---
+++ first.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+print('Namaste Bharat')
+
+joke = pyjokes.get_joke()
+
+print(joke)
+
+# REPL - READ EVALUATE PRINT LOOP 
+
+
+
---
+++ type.py
@@ -0,0 +1,5 @@
+a = '21'
+
+t = type(a)
+
+print(t)
