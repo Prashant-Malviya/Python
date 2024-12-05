cac251180e1b240508fd59a2c11b3a9b26339db6
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-12-05 14:39:54 +0530

    code updated

---
+++ virtualenv/venv.py
@@ -0,0 +1,6 @@
+# pip freeze 
+# this command give us all the environment isntalled in our python system 
+
+# pip freeze > requirement.txt 
+
+# this will give us requirement.txt file to store the environment 
